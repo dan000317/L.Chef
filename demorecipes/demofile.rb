7ea66eeb0f88ecaaf710286477d6800375aa9f02
dan000317 <dan000317@gmail.com>
2023-08-23 07:45:24 +0000

    replace single quote with double quote which encapsulates the content of the file

--- demorecipes/demofile.rb
+++ demorecipes/demofile.rb
@@ -2,6 +2,6 @@ file '/var/tmp/testfile.txt' do
   owner 'dan'
   group 'dan'
   mode '777'
-  content 'add some line of text in the file\nanonther line of text'
+  content "add some line of text in the file\nanonther line of text"
   action :create
 end
